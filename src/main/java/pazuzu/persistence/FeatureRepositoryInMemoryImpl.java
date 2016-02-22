bfad12050648ac6ec838f1422bf27c0e33360260
Jonny Velez <jonalvc@gmail.com>
2016-02-22 15:23:49 +0100

    empty impl to rid off compilation error

--- src/main/java/pazuzu/persistence/FeatureRepositoryInMemoryImpl.java
+++ src/main/java/pazuzu/persistence/FeatureRepositoryInMemoryImpl.java
@@ -1,7 +1,17 @@
 package pazuzu.persistence;
 
+import java.util.List;
+
+import pazuzu.model.Feature;
+
 /**
  * Created by cseidel on 22/02/16.
  */
 public class FeatureRepositoryInMemoryImpl implements FeatureRepository {
+
+	@Override
+	public List<Feature> getFeatures() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
