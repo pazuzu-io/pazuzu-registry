8c0f6553e5b4555b15e3a1d3dbbb774cdb0fc445
Christoph Seidel <christoph.seidel@zalando.de>
2016-02-23 14:20:57 +0100

    define FeatureService specification

--- src/main/java/pazuzu/service/FeatureService.java
+++ src/main/java/pazuzu/service/FeatureService.java
@@ -1,7 +1,35 @@
 package pazuzu.service;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import pazuzu.model.Feature;
+
+import java.util.List;
+
 /**
  * Created by cseidel on 22/02/16.
  */
 public interface FeatureService {
+
+    /**
+     * Get a List of all available features
+     *
+     * @return List of all available features
+     */
+    List<Feature> getAllFeatures();
+
+    /**
+     * Creates a topologically sorted List of Features
+     *
+     * @param featureNames List of all required featurenames
+     * @return List of topologically sorted features with their required dependencies
+     * @throws InvalidArgumentException if a requested Feature is not available
+     */
+    List<Feature> createSortedFeaturelistWithDependencies(List<String> featureNames) throws InvalidArgumentException;
+
+    /**
+     *
+     * Checks if all required features is available in the Feature Repository
+     * @return List valid features
+     */
+    List<String> validateFeatureNames(List<String> requestedFeatures);
 }
