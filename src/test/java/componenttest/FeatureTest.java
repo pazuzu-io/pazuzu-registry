405ef336ae7281529c3c61e3e37d6e43e213fdbf
Christoph Kamil Wozniak <woezelmann@gmail.com>
2016-03-11 13:24:30 +0100

    #12 initial setup for component tests

---
+++ src/test/java/componenttest/FeatureTest.java
@@ -0,0 +1,23 @@
+package componenttest;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class FeatureTest extends AbstractComponentTest {
+
+    // Just an example
+    @Test
+    public void featureref_returns_some_features() throws Exception {
+
+        ResponseEntity<List> result = template.getForEntity("http://localhost:8080/featurerefs", List.class);
+
+        Assert.assertEquals(HttpStatus.OK, result.getStatusCode());
+        Assert.assertTrue(((List<LinkedHashMap>)result.getBody()).get(0).get("name").toString().startsWith("feature"));
+    }
+}
