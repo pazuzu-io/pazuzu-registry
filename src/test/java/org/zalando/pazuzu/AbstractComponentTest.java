62bc711f22771e3aca094373ca2062211c69db9d
Christoph Kamil Wozniak <woezelmann@gmail.com>
2016-03-23 16:11:16 +0100

    #12 add method for template creation in componenttests

--- src/test/java/org/zalando/pazuzu/AbstractComponentTest.java
+++ src/test/java/org/zalando/pazuzu/AbstractComponentTest.java
@@ -1,11 +1,20 @@
 package org.zalando.pazuzu;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zalando.pazuzu.feature.FeatureFullDto;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(PazuzuAppLauncher.class)
@@ -17,7 +26,21 @@ public abstract class AbstractComponentTest {
 
     protected final TestRestTemplate template = new TestRestTemplate();
 
+    protected ObjectMapper mapper = new ObjectMapper();
+
     protected String url(String path) {
         return "http://127.0.0.1:" + port + path;
+    }
+
+    protected void createFeature(String name, String dockerData) throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", name);
+        map.put("docker_data", dockerData);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        ResponseEntity<FeatureFullDto> response = template.postForEntity(url("/api/features"), new HttpEntity<String>(mapper.writeValueAsString(map), headers), FeatureFullDto.class);
+        if (response.getStatusCode().value() > 299) throw new Exception("could not create feature " + name);
     }
 }
---
+++ src/test/java/org/zalando/pazuzu/DockerApiTest.java
@@ -0,0 +1,7 @@
+package org.zalando.pazuzu;
+
+/**
+ * Created by cwozniak on 23/03/16.
+ */
+public class DockerApiTest {
+}
