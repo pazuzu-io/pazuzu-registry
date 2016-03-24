39b680e4fd26a9218e0348e66463f688bbe032d2
David Jones <david.martin.jones@zalando.de>
2016-03-24 16:21:11 +0100

    some skeleton layout for menu

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -3,6 +3,24 @@
 const React = require('react');
 const client = require('./client');
 
+var data = [
+	{id: 0, text: "Create Container"},
+	{id: 2, text: "View Features"}
+];
+
+var PazuzuApp = React.createClass({displayName: 'PazuzuApp',
+	render: function() {
+		return (
+			React.createElement('div', {className: 'pazuzuApp'},
+			<div>
+			  <Header />	
+			  <Menu data={this.props.data} />
+			</div>		
+)
+		)
+}
+})
+
 var HeaderMenu = React.createClass({displayName: 'HeaderMenu',
 	render: function() {
 		return (
@@ -16,9 +34,7 @@ var HeaderMenu = React.createClass({displayName: 'HeaderMenu',
 var HeaderZalando = React.createClass({displayName: 'HeaderZalando',
 	render: function() {
 		return (
-		React.createElement('div', {className: 'headerZalando'},
-			"ZALANDO"
-			)
+		    React.createElement('div', {className: 'headerZalando'}, "ZALANDO")
 		);
 	}
 });
@@ -35,10 +51,33 @@ var Header = React.createClass({displayName: 'Header',
 	}
 });
 
+var Menu = React.createClass({displayName: 'Menu',
+	render: function() {
+	var menuNodes = this.props.data.map(function(item) {
+		return (
+			<MenuItem key={item.id} text={item.text}> 
+				
+			</MenuItem>
+		);
+	});
+	return (
+	  <div className="menu">
+		  {menuNodes}
+	  </div>
+	);
+	}
+});
+
+var MenuItem = React.createClass({displayName: 'MenuItem',
+	render: function() {
+		return (<div className="menuItem">{this.props.text}</div>)
+	}
+})
+
 React.render(
-	React.createElement(Header, null),
+	<PazuzuApp data={data} />,
 	document.getElementById('react')
-)
+);
 
 
 
