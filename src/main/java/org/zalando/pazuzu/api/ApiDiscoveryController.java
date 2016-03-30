a2c54903a1d8c61cefc31c4b67892f09bbe4492b
Felix Müller <felix.mueller2@zalando.de>
2016-03-30 17:04:33 +0200

    fixed api discovery resource for bundled executable jar

--- src/main/java/org/zalando/pazuzu/api/ApiDiscoveryController.java
+++ src/main/java/org/zalando/pazuzu/api/ApiDiscoveryController.java
@@ -1,14 +1,14 @@
-package org.zalando.pazuzu;
+package org.zalando.pazuzu.api;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Controller
 @CrossOrigin
@@ -16,8 +16,8 @@ public class ApiDiscoveryController {
 
     @RequestMapping(value = "/api/swagger.yaml")
     @ResponseBody
-    public String serveSwaggerApiDefinition(HttpServletResponse response) throws IOException {
+    public String serveSwaggerApiDefinition(HttpServletResponse response) throws IOException, URISyntaxException {
         response.setContentType("text/x-yaml");
-        return FileUtils.readFileToString(ResourceUtils.getFile("classpath:api/swagger.yaml"), "UTF-8");
+        return IOUtils.toString(this.getClass().getResourceAsStream("/api/swagger.yaml"), "UTF-8");
     }
 }
