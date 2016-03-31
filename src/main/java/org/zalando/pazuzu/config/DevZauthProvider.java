3bb6a45db44deac78db1f32261bd44eb10e6df4b
Moises Trovo <moises.trovo@gmail.com>
2016-03-31 11:11:00 +0200

    adding ZAuth mock bean for development environment

---
+++ src/main/java/org/zalando/pazuzu/config/DevZauthProvider.java
@@ -0,0 +1,35 @@
+package org.zalando.pazuzu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.social.zauth.api.ZAuth;
+import org.springframework.social.zauth.api.impl.ZAuthTemplate;
+import org.zalando.zauth.teams.TeamsOperations;
+import org.zalando.zauth.users.UsersOperations;
+
+@Configuration
+@Profile("dev")
+public class DevZauthProvider {
+
+    @Bean
+    public ZAuth zAuth() {
+        return new ZAuth() {
+            @Override
+            public String getCurrentLogin() {
+                return "devuser";
+            }
+
+            @Override
+            public UsersOperations userOperations() {
+                return null;
+            }
+
+            @Override
+            public TeamsOperations teamsOperations() {
+                return null;
+            }
+        };
+    }
+}
+
