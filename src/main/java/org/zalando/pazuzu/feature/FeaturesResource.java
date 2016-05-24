1bd4084fe3d2ca13eacee9e7d040afb1b2261319
indy256 <indy256@gmail.com>
2016-05-24 15:41:01 +0200

    pazuzu-registry-73 Replaced star-imports with specific imports

--- src/main/java/org/zalando/pazuzu/feature/FeaturesResource.java
+++ src/main/java/org/zalando/pazuzu/feature/FeaturesResource.java
@@ -3,18 +3,14 @@ package org.zalando.pazuzu.feature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.zalando.pazuzu.exception.ServiceException;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @CrossOrigin
