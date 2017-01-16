8a264ed5aec6f618990f43ac560dec445e67e938
falmp <chico.lopes@gmail.com>
2017-01-16 11:41:15 +0100

    Add status field to Feature

--- src/main/resources/db/migration/features/V1_1_1__java.sql
+++ src/main/resources/db/migration/features/V1_1_1__java.sql
@@ -1,5 +1,5 @@
-INSERT INTO FEATURE (NAME, DESCRIPTION, CREATED_AT, UPDATED_AT, AUTHOR, SNIPPET, TEST_SNIPPET) VALUES
-  ('java', 'basic java feature', now(), now(), 'Pazuzu team', 'RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
+INSERT INTO FEATURE (NAME, DESCRIPTION, CREATED_AT, UPDATED_AT, STATUS, AUTHOR, SNIPPET, TEST_SNIPPET) VALUES
+  ('java', 'basic java feature', now(), now(), 'APPROVED', 'Pazuzu team', 'RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
 RUN echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 RUN echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 
--- src/main/resources/db/migration/features/V1_1_2__leinigen.sql
+++ src/main/resources/db/migration/features/V1_1_2__leinigen.sql
@@ -1,5 +1,5 @@
-INSERT INTO FEATURE (NAME, DESCRIPTION, CREATED_AT, UPDATED_AT, AUTHOR, SNIPPET, TEST_SNIPPET) VALUES
-  ('leiningen', 'demo feature with no dependency', now(), now(), 'Pazuzu team', 'RUN wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -O /usr/bin/lein \
+INSERT INTO FEATURE (NAME, DESCRIPTION, CREATED_AT, UPDATED_AT, STATUS, AUTHOR, SNIPPET, TEST_SNIPPET) VALUES
+  ('leiningen', 'demo feature with no dependency', now(), now(), 'PENDING', 'Pazuzu team', 'RUN wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -O /usr/bin/lein \
     && chmod +x /usr/bin/lein', '#!/usr/bin/env bats
 
 @test "Check that Leiningen is installed" {
