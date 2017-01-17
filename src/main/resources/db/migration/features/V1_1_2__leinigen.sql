e20e5fb138c76baa4b5e311922c5244d097ff040
François Hertay <frank124@gmail.com>
2017-01-17 11:32:26 +0100

    PAZUZU-REGISTRY-120 Clean DB creation… (#126)
    * PAZUZU-REGISTRY-120 Clean DB creaton and population with default features.
    * Move the example meta features to dev application profile only.

---
+++ src/main/resources/db/migration/features/V1_1_2__leinigen.sql
@@ -0,0 +1,7 @@
+INSERT INTO FEATURE (NAME, DESCRIPTION, CREATED_AT, UPDATED_AT, AUTHOR, SNIPPET, TEST_SNIPPET) VALUES
+  ('leiningen', 'demo feature with no dependency', now(), now(), 'Pazuzu team', 'RUN wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -O /usr/bin/lein \
+    && chmod +x /usr/bin/lein', '#!/usr/bin/env bats
+
+@test "Check that Leiningen is installed" {
+    command lein -v
+}');
