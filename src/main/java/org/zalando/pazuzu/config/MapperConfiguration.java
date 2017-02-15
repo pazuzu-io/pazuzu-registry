708e65051196ac6fa64d2404a67f39ee6d8a3b48
Oliver Trosien <oliver.trosien@zalando.de>
2017-02-15 17:18:21 +0100

    Upgrade twintip-spring-web to 1.1.0
    This causes to excplicitly having to add
    jackson-jdk8 and dealing with the fact that swagger json is now
    indented.

--- src/main/java/org/zalando/pazuzu/config/MapperConfiguration.java
+++ src/main/java/org/zalando/pazuzu/config/MapperConfiguration.java
@@ -1,28 +1,31 @@
 package org.zalando.pazuzu.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.zalando.problem.ProblemModule;
-
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.zalando.problem.ProblemModule;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
 @Configuration
 public class MapperConfiguration {
+
     @Bean
-    public ObjectMapper objectMapper() {
+    @Primary
+    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        return new ObjectMapper()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .setDateFormat(dateFormat)
-                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                .registerModule(new Jdk8Module())
-                .registerModule(new ProblemModule());
+        return builder
+                .failOnUnknownProperties(false)
+                .dateFormat(dateFormat)
+                .serializationInclusion(Include.NON_NULL)
+                .modules(new Jdk8Module(), new ProblemModule());
     }
 
 }
